e799811a40cd5d73ee54d0660990f3212e8f49a3
Somer Hayter <me@somer.io>
2020-03-14 13:43:15 +1100

    Fix compiling tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
+  roots: [
+    "src"
+  ],
   collectCoverageFrom: [
     "src/**/*.{js,jsx,ts,tsx}"
   ]
